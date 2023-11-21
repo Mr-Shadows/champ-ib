1db698984dfc548c0e94d9978f5379fe1e295a41
Oleg <oleg-kruglenko@mail.ru>
2023-11-21 04:29:00 -0500

    init conf

--- terraform/conf.tf
+++ terraform/conf.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
 variable "cloud_id" {
   description = "b1gtrb1pnt0mnhlbb7nc"
   type        = string
